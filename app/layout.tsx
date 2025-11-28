21d8eaa64f3ba382e0b450b6b39b7214b1efddbd
chenhong007 <ahong007@yeah.net>
2025-11-28 21:49:18 +0800

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { ToastProvider } from '@/components/shared/ToastContainer'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="zh-CN">
       <body className={inter.className}>
         <ToastProvider>{children}</ToastProvider>
+        <Analytics />
       </body>
     </html>
   )
